5ac990317288a72d50f39d8fcb4486b14dfddc9e
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-11-06 08:17:48 -0300

    Updates

---
+++ precache-manifest.ab2976ebd3942d2ffd9f406c98be988c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a06ed2faa69d031a4ba78940163221a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "718fd0dc04290d59e97b",
+    "url": "/static/js/2.0f4f4fb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0f4f4fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61682ea80e161bc1c2a9",
+    "url": "/static/js/main.a3d840e3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b8a81e51ee8ea2d1cb3e24a52a0b11c6",
+    "url": "/static/media/my-photo.b8a81e51.png"
+  }
+]);
