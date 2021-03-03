aba9004275e3913df13b4360171164b94c3a762a
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-03-03 15:52:51 -0300

    chore: change text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,7 +17,7 @@ const About = () => (
 
                 <p>Tenho contato com a área de programação desde dos meus 13 anos e, assim, pude ter contato com o desenvolvimento mobile e web, mas sempre gostei do desenvolvimento Android nativo com Java e Kotlin. Dessa forma, me concentrei em aprender as principais arquiteturas, bibliotecas e boas práticas de codificação. Sou um Dev que ama ter um código organizado para fácil manutenção e entendimento.</p>
 
-                <p>Atualmente, sou Desenvolvedor Android na Bornlogic que é empresa de tecnologia com foco em performance em anunciantes digitais e sendo parceira de Negócios e AdTech do Facebook, Google e Tableau.</p>
+                <p>Atualmente, sou Desenvolvedor Android na Bornlogic que é uma empresa de tecnologia com foco em performance em anunciantes digitais e sendo parceira de Negócios e AdTech do Facebook, Google e Tableau.</p>
 
                 <p>Desde do começo da minha trajetória, criei o perfil @alvarenga.dev no instagram com o intuito de compartilhar conhecimento e ajudar outros a começarem com o “pé direito”.</p>
             </Content>
